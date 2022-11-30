c056f2f2ea3485830a43c655ae2f8293c36c1a28
cosy247 <705194673@qq.com>
2022-11-30 17:09:53 +0800

    refactor: 作为npm项目包

--- test/src/js/2.js
+++
@@ -1,3 +0,0 @@
-export function jian(a,b) {
-  return a-b;
-}
--- test/src/main.js
+++
@@ -1,6 +0,0 @@
-import { add } from "./js/1.js";
-import { jian } from "./js/2.js";
-import "./css/1.css";
-
-console.log(add(1,2));
-console.log(jian(1,2));
